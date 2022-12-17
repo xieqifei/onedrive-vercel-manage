96126f1d643b67de24efe6a504e9efc0ffc405ff
xieqifei <me@xieqifei.com>
2022-12-17 12:45:52 +0100

    redis quit

--- utils/odAuthTokenStore.ts
+++ utils/odAuthTokenStore.ts
@@ -3,12 +3,13 @@ import siteConfig from '../config/site.config'
 
 // Persistent key-value store is provided by Redis, hosted on Upstash
 // https://vercel.com/integrations/upstash
-const kv = new Redis(process.env.REDIS_URL || '')
+
 
 export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refreshToken: unknown }> {
+  const kv = new Redis(process.env.REDIS_URL || '')
   const accessToken = await kv.get(`${siteConfig.kvPrefix}access_token`)
   const refreshToken = await kv.get(`${siteConfig.kvPrefix}refresh_token`)
-
+  kv.quit()
   return {
     accessToken,
     refreshToken,
@@ -24,6 +25,8 @@ export async function storeOdAuthTokens({
   accessTokenExpiry: number
   refreshToken: string
 }): Promise<void> {
+  const kv = new Redis(process.env.REDIS_URL || '')
   await kv.set(`${siteConfig.kvPrefix}access_token`, accessToken, 'EX', accessTokenExpiry)
   await kv.set(`${siteConfig.kvPrefix}refresh_token`, refreshToken)
+  kv.quit()
 }
