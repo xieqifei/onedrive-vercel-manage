3f3dc303e5c383e6818127f55376c7fb7380de4b
xieqifei <me@xieqifei.com>
2022-12-20 18:45:06 +0100

    site

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "ShareDrive",
+  title: "onedrive-vercel-manage",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/public_share',
